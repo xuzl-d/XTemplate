ee876caab48aff0f13577dad1b68609fc847d7ae
xuzl-d <69352418+xuzl-d@users.noreply.github.com>
2025-04-19 18:03:57 +0800

    print

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 int main()
 {
     XTP::print(1.0, 2, "hello", 2.5f);
-
+    XTP::print2(1,2.0,"word",2.4f,55);
     return 0;
 }
--- print.hpp
+++ print.hpp
@@ -4,6 +4,7 @@
 
 namespace XTP
 {
+    //打印任意参数
     template <typename T>
     void print(T val)
     {
@@ -13,9 +14,20 @@ namespace XTP
     template<typename T, typename... Types>
     void print(T val, Types... arg)
     {
-        std::cout<<val<<std::endl;
+        print(val);
         print(arg...);
     }
+
+    //利用sizeof... 得到参数数量；折叠表达式展开参数
+    template<typename... Types>
+    void print2(Types... args)
+    {
+        //std::cout << sizeof...(Types) << std::endl; //输出剩余参数数量
+        //(std::cout << ... << args) << std::endl; //直接输出无法分隔
+        //(print(args), ...);
+        (..., print(args));
+    }
+
 
 } // namespace XTP
 
